b149d6e5b08217c2d3ce21d3cf62c6ade9d7e26f
Kajonhkiat <kajorhkaen@gmail.com>
2023-06-14 16:03:22 +0700

    Add: template  literal

--- GO/future_skill/hello.go
+++ GO/future_skill/hello.go
@@ -5,5 +5,19 @@ import (
 )
 
 func main() {
-	fmt.Println("hello")
+	// variable out scoop
+	// var name string = "hello"
+
+	//variable in scoop function
+	name := "hello"
+	fmt.Println(name)
+
+	//mutiple declaration
+	price, age, check := 200, 20, true
+	fmt.Println(price, age, check)
+
+	//template  literal
+	result := fmt.Sprintf("have %d THB", price)
+
+	fmt.Printf(result)
 }
