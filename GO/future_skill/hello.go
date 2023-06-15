410aa0485bf78f8444b53e02c80a1169dc8ec7f7
Kajonhkiat <kajorhkaen@gmail.com>
2023-06-15 09:12:57 +0700

    Add: formatting

--- GO/future_skill/hello.go
+++ GO/future_skill/hello.go
@@ -16,8 +16,37 @@ func main() {
 	price, age, check := 200, 20, true
 	fmt.Println(price, age, check)
 
+	//raw string
+	data := `1:a 
+2:b
+3:c`
+	fmt.Println(data)
+
 	//template  literal
 	result := fmt.Sprintf("have %d THB", price)
-
 	fmt.Printf(result)
+
+	var char rune = 'A'
+	fmt.Println("char:", char)
+
+	//Fromatting
+	mock := 123
+
+	//string & new line
+	fmt.Println("")
+
+	//format string
+	fmt.Printf("mock: %d", mock)
+
+	// %s ใช้สำหรับแทนที่ string
+	// %d ใช้สำหรับแทนที่ integer
+	// %f (float)
+	// %.2f(float 2 position)
+	// %t (boolean)
+	// %v (ค่าประเภทอื่นๆ)
+	// %#v all type
+	// %c character
+	// \n new line
+	// %T find type
+
 }
